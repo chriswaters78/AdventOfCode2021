8208eb22aef04895a9e26e1bb8453d205f56ef04
Christian Waters <cwaters@truenorthit.co.uk>
2021-12-01 16:03:49 +0000

    first day

---
+++ Advent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = File.ReadAllLines(args[0])
+                .Select(int.Parse)
+                .Aggregate(new { LastReading = int.MaxValue, Count = 0 }, (acc, currReading) => new { LastReading = currReading, Count = currReading > acc.LastReading ? acc.Count + 1 : acc.Count })
+                .Count;
+        }
+    }
+}
---
+++ Advent1b/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines(args[0])
+                .Select(int.Parse).ToList();
+
+            var slidingTotals = numbers.Take(numbers.Count - 2).Select((value, index) => numbers.Skip(index).Take(3).Sum());
+            var answer = slidingTotals
+                .Aggregate(new { LastReading = int.MaxValue, Count = 0 }, (acc, currReading) => new { LastReading = currReading, Count = currReading > acc.LastReading ? acc.Count + 1 : acc.Count })
+                .Count;
+        }
+    }
+}
