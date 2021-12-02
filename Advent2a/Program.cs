106680f72e1b49910c205adf35a5fa21853cb828
Christian Waters <cwaters@truenorthit.co.uk>
2021-12-02 10:22:04 +0000

    day 2

---
+++ Advent2a/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, int)> instructions = File.ReadAllLines(args[0]).Select(line => line.Split(' ')).Select(arr => (arr[0], int.Parse(arr[1]))).ToList();
+
+            int hor = 0;
+            int depth = 0;
+            foreach (var (dir, value) in instructions)
+            {
+                switch (dir)
+                {
+                    case "forward":
+                        hor += value;
+                        break;
+                    case "down":
+                        depth += value;
+                        break;
+                    case "up":
+                        depth -= value;
+                        break;
+                }
+            }
+
+            var result = hor * depth;
+        }
+    }
+}
---
+++ Advent2b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, int)> instructions = File.ReadAllLines(args[0]).Select(line => line.Split(' ')).Select(arr => (arr[0], int.Parse(arr[1]))).ToList();
+
+            int hor = 0;
+            int depth = 0;
+            int aim = 0;
+            foreach (var (dir, value) in instructions)
+            {
+                switch (dir)
+                {
+                    case "forward":
+                        hor += value;
+                        depth += aim * value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+            }
+
+            var result = hor * depth;
+        }
+    }
+}
