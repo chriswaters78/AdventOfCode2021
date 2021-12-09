3420872218a18424f0dbb19995cbf78af9ef85a5
Christian Waters <cwaters@truenorthit.co.uk>
2021-12-09 09:59:59 +0000

    Day 9

--- Advent9a/Program.cs
+++ Advent9a/Program.cs
@@ -19,21 +19,54 @@ namespace Advent9a
                 .Concat(new[] { Enumerable.Repeat(int.MaxValue, lineLength + 2) }).Select(ie => ie.ToList()).ToList();
 
             List<int> lowPoints = new List<int>();
-            for (int i = 1; i < noLines; i++)
+            List<HashSet<(int, int)>> basins = new List<HashSet<(int, int)>>();
+            for (int i = 1; i <= noLines; i++)
             {
-                for (int j = 1; j < lineLength; j++)
+                for (int j = 1; j <= lineLength; j++)
                 {
-                    if (input[i-1][j-1] > input[i][j]
-                        && input[i + 1][j - 1] > input[i][j]
-                        && input[i - 1][j + 1] > input[i][j]
-                        && input[i + 1][j + 1] > input[i][j])
+                    if (input[i-1][j] > input[i][j]
+                        && input[i + 1][j] > input[i][j]
+                        && input[i][j - 1] > input[i][j]
+                        && input[i][j + 1] > input[i][j])
                     {
                         lowPoints.Add(input[i][j]);
+                        basins.Add(new HashSet<(int, int)>() { (i, j) });
                     }
                 }
             }
 
             var answer = lowPoints.Sum(i => i + 1);
+
+            //expand each basin
+            for (int c = 0; c < basins.Count; c++)
+            {
+                while (true)
+                {
+                    var expandedBasin = new HashSet<(int, int)>(basins[c]);
+                    foreach (var (i, j) in basins[c])
+                    {
+                        var candidates = new[] { (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1 )};
+                        foreach (var (ci,cj) in candidates)
+                        {
+                            if (input[ci][cj] > input[i][j] && input[ci][cj] != int.MaxValue && input[ci][cj] != 9)
+                            {
+                                if (!expandedBasin.Contains((ci,cj)))
+                                {
+                                    expandedBasin.Add((ci, cj));
+                                }
+                            }
+                        }
+                    }
+
+                    if (expandedBasin.Count == basins[c].Count)
+                    {
+                        break;
+                    }
+                    basins[c] = expandedBasin;
+                }
+            }
+
+            var answer2 = basins.OrderByDescending(basin => basin.Count).Take(3).Aggregate(1, (total, hs) => total * hs.Count);
         }
     }
 }
