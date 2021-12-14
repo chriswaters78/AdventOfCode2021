67e2063309396df8c5330d5b12007ea42eb77d7f
Christian Waters <cwaters@truenorthit.co.uk>
2021-12-14 11:34:51 +0000

    day 14

---
+++ Advent14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Advent14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+            var template = new LinkedList<char>(lines[0]);
+            var rules = lines.Skip(2).Select(line => { var split = line.Split(' '); return (split[0], split[2][0]); });
+
+
+            for (int step = 0; step < 40; step++)
+            {
+                var currentNode = template.First;
+                List<(LinkedListNode<char>, char)> inserts = new List<(LinkedListNode<char>, char)>();
+                while (currentNode.Next != null)
+                {
+                    foreach (var rule in rules)
+                    {
+                        if (rule.Item1[0] == currentNode.Value && rule.Item1[1] == currentNode.Next.Value)
+                        {
+                            inserts.Add((currentNode, rule.Item2));
+                        }
+                    }
+
+                    currentNode = currentNode.Next;
+                }
+
+                foreach (var insert in inserts)
+                {
+                    var next = insert.Item1.Next;
+                    template.AddAfter(insert.Item1, insert.Item2);                    
+                }
+            }
+
+            var sorted = template.GroupBy(ch => ch).Select(grp => (grp.Key, grp.Count())).OrderBy(tp => tp.Item2).ToList();
+            var answer = sorted.Last().Item2 - sorted.First().Item2;
+        }
+    }
+}
---
+++ Advent14b/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+namespace Advent14b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+            var template = new LinkedList<char>(lines[0]);
+            var rules = lines.Skip(2).Select(line => { var split = line.Split(' '); return (split[0], split[2][0]); });
+
+            Dictionary<string, long> pairCounts = new Dictionary<string, long>();
+            var currentNode = template.First;
+            while (currentNode.Next != null)
+            {
+                var pair = new String(new[] { currentNode.Value, currentNode.Next.Value });
+                if (!pairCounts.ContainsKey(pair))
+                {
+                    pairCounts[pair] = 0;
+                }
+                pairCounts[pair] += 1;
+
+                currentNode = currentNode.Next;
+            }
+
+            Dictionary<char, long> charactersAdded = new Dictionary<char, long>();
+            for (int step = 0; step < 40; step++)
+            {
+                List<(string, long)> additions = new List<(string, long)>();
+                foreach (var rule in rules)
+                {
+                    if (pairCounts.ContainsKey(rule.Item1) && pairCounts[rule.Item1] > 0)
+                    {
+                        //e.g. say we have BC -> D a total of N times
+                        //we set BC count to 0
+                        //we add BD x N
+                        //we add DC x N
+
+                        var newPair1 = new String(new[] { rule.Item1[0], rule.Item2 });
+                        var newPair2 = new String(new[] { rule.Item2, rule.Item1[1]});
+
+                        additions.Add((newPair1, pairCounts[rule.Item1]));
+                        additions.Add((newPair2, pairCounts[rule.Item1]));
+
+                        if (!charactersAdded.ContainsKey(rule.Item2))
+                        {
+                            charactersAdded[rule.Item2] = 0;
+                        }
+                        charactersAdded[rule.Item2] += pairCounts[rule.Item1];
+                        pairCounts[rule.Item1] = 0;
+                    }
+                }
+
+                foreach (var addition in additions)
+                {
+                    if (!pairCounts.ContainsKey(addition.Item1))
+                    {
+                        pairCounts[addition.Item1] = 0;
+                    }
+                    pairCounts[addition.Item1] += addition.Item2;
+                }
+            }
+
+            foreach (var c in template)
+            {
+                if (!charactersAdded.ContainsKey(c))
+                {
+                    charactersAdded[c] = 0;
+                }
+                charactersAdded[c] += 1;
+            }
+
+            var characterCounts = charactersAdded.Select(kvp => kvp.Value).ToList();
+            var answer = characterCounts.Max() - characterCounts.Min();
+
+        }
+    }
+}
