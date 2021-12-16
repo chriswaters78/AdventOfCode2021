5ea89b7a57722278efb1a674c2a2012aade04f8a
Christian Waters <cwaters@truenorthit.co.uk>
2021-12-16 17:02:12 +0000

    neater 16

--- Advent16/Program.cs
+++ Advent16/Program.cs
@@ -10,59 +10,29 @@ namespace Advent16
 {
     class Program
     {
-        static long versionTotal = 0;
 
         static void Main(string[] args)
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            var hex = File.ReadAllText(args[0]);
-            var bits = new bool[hex.Length * 4];
+            var bits = File.ReadAllText(args[0])
+                    .Aggregate(new StringBuilder(), (sb, ch) => { sb.Append(Convert.ToString(Convert.ToInt32(ch.ToString(), 16), 2).PadLeft(4, '0')); return sb; }, sb => sb.ToString())
+                    .Select(ch => ch == '1' ? true : false).ToArray();
 
-            int c = 0;
-            foreach (var ch in hex)
-            {
-                var i = Convert.ToInt32(ch.ToString(), 16);
-                var binary = Convert.ToString(i, 2).PadLeft(4,'0');
-                foreach (var chb in binary)
-                {
-                    bits[c] = chb == '1' ? true : false;
-                    c += 1;
-                }
-            }
-
-            int cps = 0;
-
-            while (true)
-            {
-                (int cps2, long val2) = getNextPacket(bits[0..]);
-                cps += cps2;
-
-                if (cps >= hex.Length - 3 || (!bits[cps] && !bits[cps + 1] && !bits[cps + 1]))
-                {
-                    //no more packets
-                    break;
-                }
-            }
-
+            (_, long answer1, long answer2) = getNextPacket(bits[0..]);
 
             watch.Stop();
-
-
-
-            Console.WriteLine($"Part 1: {0}");
+            Console.WriteLine($"Part 1: {answer1}");
+            Console.WriteLine($"Part 2: {answer2}");
             Console.WriteLine($"Elapsed Time: {watch.ElapsedMilliseconds}ms");
         }
 
-        private static (int bitsConsumed, long value) getNextPacket(bool[] bits)
+        private static (int bitsConsumed, long versionTotal, long value) getNextPacket(bool[] bits)
         {
-            long value = -1;
+            long value;
             int cps = 0;
-            //first 3 bits are version
-            long version = getIntFromArray(bits[cps..(cps + 3)]);
-            versionTotal += version;
-
+            long version = getIntFromArray(bits[cps..(cps + 3)]);            
             cps += 3;
 
             long type = getIntFromArray(bits[cps..(cps + 3)]);
@@ -71,90 +41,71 @@ namespace Advent16
             if (type == 4)
             {
                 int totalLiteralBits = 0;
-                int groups = 0;
                 //110 100 10111 11110 00101 000
                 //literal, 5 bits groups, first bit 1 until last one where first bit is 0
                 List<bool> literal = new List<bool>();
-                while (bits[cps + totalLiteralBits])
+                do
                 {
                     literal.AddRange(bits[(cps + totalLiteralBits + 1)..(cps + totalLiteralBits + 5)]);
                     totalLiteralBits += 5;
-                    groups++;
                 }
-                literal.AddRange(bits[(cps + totalLiteralBits + 1)..(cps + totalLiteralBits + 5)]);
-                totalLiteralBits += 5;
-                groups++;
+                while (bits[cps + totalLiteralBits - 5]);
 
                 value = getIntFromArray(literal.ToArray());
-                if ((groups * 4) % 4 != 0)
+                if ((totalLiteralBits / 5 * 4) % 4 != 0)
                 {
-                    totalLiteralBits += 4 - ((groups * 4) % 4);
+                    totalLiteralBits += 4 - ((totalLiteralBits / 5 * 4) % 4);
                 }
 
                 cps += totalLiteralBits;
             }
             else
             {
-                List<long> values = new List<long>();
+                List<(int bitsConsumed, long versionTotal, long value)> packets = new List<(int, long, long)>();
+                cps++;
                 //operator
-                if (bits[cps])
+                if (bits[cps-1])
                 {
                     //length type 1, number of sub packets
-                    cps++;
                     long numberSubPackets = getIntFromArray(bits[cps..(cps + 11)]);
                     cps += 11;
 
                     for (int i = 0; i < numberSubPackets; i++)
                     {
-                        (int cps2, long val2) = getNextPacket(bits[cps..]);
-                        values.Add(val2);
-                        cps += cps2;
+                        var packet = getNextPacket(bits[cps..]);
+                        packets.Add(packet);
+                        cps += packet.bitsConsumed;
                     }
-                    //done with this sub-packet
                 }
                 else
                 {
-                    //length type 0, total length
-                    cps++;
-                    long length = getIntFromArray(bits[cps..(cps + 15)]);
+                    long packetLength = getIntFromArray(bits[cps..(cps + 15)]);
                     cps += 15;
                     int totalSubPLength = 0;
-                    while (totalSubPLength < length)
+                    while (totalSubPLength < packetLength)
                     {
-                        (int cps2, long val2) = getNextPacket(bits[(cps + totalSubPLength)..]);
-                        values.Add(val2);
-                        totalSubPLength += cps2;
+                        var packet = getNextPacket(bits[(cps + totalSubPLength)..]);
+                        packets.Add(packet);
+                        totalSubPLength += packet.bitsConsumed;
                     }
                     cps += totalSubPLength;
                 }
 
-                switch (type)
+                version += packets.Sum(pk => pk.versionTotal);
+
+                value = type switch
                 {
-                    case 0:
-                        value = values.Sum();
-                        break;
-                    case 1:
-                        value = values.Aggregate(1L, (ac, v) => ac * v);
-                        break;
-                    case 2:
-                        value = values.Min();
-                        break;
-                    case 3:
-                        value = values.Max();
-                        break;
-                    case 5:
-                        value = values.First() > values.Skip(1).First() ? 1 : 0;
-                        break;
-                    case 6:
-                        value = values.First() < values.Skip(1).First() ? 1 : 0;
-                        break;
-                    case 7:
-                        value = values.First() == values.Skip(1).First() ? 1 : 0;
-                        break;
-                }
+                    0 => packets.Select(pk => pk.value).Sum(),
+                    1 => packets.Select(pk => pk.value).Aggregate(1L, (ac, v) => ac * v),
+                    2 => value = packets.Select(pk => pk.value).Min(),
+                    3 => packets.Select(pk => pk.value).Max(),
+                    5 => packets.First().value > packets.Skip(1).First().value ? 1 : 0,
+                    6 => packets.First().value < packets.Skip(1).First().value ? 1 : 0,
+                    _ => packets.First().value == packets.Skip(1).First().value ? 1 : 0
+                };
             }
 
-            return (cps, value);
+            return (cps, version, value);
         }
 
         private static long getIntFromArray(bool[] bits)
