019e4861a58a8f2adeda131f557bf4c06abf639e
Christian Waters <cwaters@truenorthit.co.uk>
2021-12-19 00:12:14 +0000

    tidied up
    tidy

--- Advent18/Program.cs
+++ Advent18/Program.cs
@@ -1,64 +1,54 @@
 ﻿using System.Diagnostics;
 using System.Text;
 
-Stopwatch watch = new Stopwatch();
+const bool debug = false;
+
+var watch = new Stopwatch();
 watch.Start();
+
 var lines = File.ReadAllLines(args[0]).ToList();
-var addAll = lines.Select(line => Parse(line)).Aggregate((op1, op2) => Add(op1, op2));
+var answer1 = lines.Select(line => Tree.Parse(line)).Aggregate((op1, op2) => add(op1, op2)).Magnitude();
 
-var answer1 = GetMag(addAll);
+var answer2 = (
+     from i in Enumerable.Range(0, lines.Count)
+     from j in Enumerable.Range(0, lines.Count)
+     where i != j
+     select add(Tree.Parse(lines[i]), Tree.Parse(lines[j])).Magnitude())
+.Max();
 
-List<Tree> additions = new List<Tree>();
-for (int i = 0; i < lines.Count; i++)
-{
-    for (int j = 0; j < lines.Count; j++)
-    {
-        if (i != j)
-        {
-            additions.Add(Add(Parse(lines[i]), Parse(lines[j])));
-        }
-    }
-}
 watch.Stop();
 Console.WriteLine($"Answer1: {answer1}");
-Console.WriteLine($"Answer2: {additions.Max(tree => GetMag(tree))}");
+Console.WriteLine($"Answer2: {answer2}");
 Console.WriteLine($"Elapsed: {watch.ElapsedMilliseconds}ms");
 
-static int GetMag(Tree tree)
-{
-    if (tree.Left == null && tree.Right == null)
-    {
-        return tree.Value;
-    }
-    return 3 * GetMag(tree.Left) + 2 * GetMag(tree.Right);
-}
 
-
-static Tree Add(Tree op1, Tree op2)
+static Tree add(Tree op1, Tree op2)
 {
     Tree tree = new Tree();
     tree.Left = op1;
     tree.Right = op2;
     op1.Parent = tree;
-    op1.IsLeft = true;
     op2.Parent = tree;
+    op1.IsLeft = true;
     op2.IsRight = true;
 
-    //Console.WriteLine($"{op1}");
-    //Console.WriteLine($"+ {op2}");
-
-    //Console.WriteLine($"after addition: {tree}");
+    if (debug)
+    {
+        Console.WriteLine($"{op1}");
+        Console.WriteLine($"+ {op2}");
+        Console.WriteLine($"after addition: {tree}");
+    }
 
     while (true)
     {
-        if (Explode(tree, 0))
+        if (explode(tree, 0))
         {
-            //Console.WriteLine($"after explode:  {tree}");
+            if (debug) Console.WriteLine($"after explode:  {tree}");
             continue;
         }
-        if (Split(tree))
+        if (split(tree))
         {
-            //Console.WriteLine($"after split:    {tree}");
+            if (debug) Console.WriteLine($"after split:    {tree}");
             continue;
         }
         break;
@@ -67,46 +57,18 @@ static Tree Add(Tree op1, Tree op2)
     return tree;
 }
 
-
-
-static bool Explode(Tree tree, int depth)
+static bool explode(Tree tree, int depth)
 {
-    if (depth >= 5 && tree.Left == null && tree.Right == null)
+    if (depth == 5)
     {
-        var curr = tree;
-        while (curr != null && curr.IsLeft)
-        {
-            curr = curr.Parent;
+        foreach ((Tree from, Tree next)  in new[] { (tree, tree.FindLeft()), (tree.Parent.Right,tree.Parent.Right.FindRight()) })
+        {                
+            if (next != null)
+            {
+                next.Value += from.Value;
+            }
         }
-        curr = curr?.Parent?.Left;
-
-        while (curr?.Right != null)
-        {
-            curr = curr.Right;
-        }
-
-        if (curr != null)
-        {
-            curr.Value += tree.Value;
-        }
-
-        curr = tree.Parent.Right;
-        while (curr != null && curr.IsRight)
-        {
-            curr = curr.Parent;
-        }
-        curr = curr?.Parent?.Right;
-
-        while (curr?.Left != null)
-        {
-            curr = curr.Left;
-        }
-
-        if (curr != null)
-        {
-            curr.Value += tree.Parent.Right.Value;
-        }
-
+        
         tree = tree.Parent;
         tree.Left = null;
         tree.Right = null;
@@ -114,24 +76,21 @@ static bool Explode(Tree tree, int depth)
 
         return true;
     }
-    else
+    if (tree.Left != null && explode(tree.Left, depth + 1))
     {
-        if (tree.Left != null && Explode(tree.Left, depth + 1))
-        {
-            return true;
-        }
-        if (tree.Right != null)
-        {
-            return Explode(tree.Right, depth + 1);
-        }
-        return false;
+        return true;
     }
+    if (tree.Right != null)
+    {
+        return explode(tree.Right, depth + 1);
+    }
+    return false;
 }
 
-static bool Split(Tree tree)
+static bool split(Tree tree)
 {
     Stack<Tree> stack = new Stack<Tree>(new[] { tree });
-    while (stack.Any())
+    while (stack.Count > 0)
     {
         var curr = stack.Pop();
         if (curr.Right != null)
@@ -143,20 +102,17 @@ static bool Split(Tree tree)
             stack.Push(curr.Left);
         }
 
-        if (curr.Left == null && curr.Right == null && curr.Value >= 10)
+        if (curr.Value >= 10)
         {
-            Tree split1 = new Tree();
-            Tree split2 = new Tree();
-            split1.IsLeft = true;
-            split2.IsRight = true;
-            split1.Value = curr.Value / 2;
-            split2.Value = (curr.Value + 1) / 2;
-            split1.Parent = curr;
-            split2.Parent = curr;
-
-            curr.Left = split1;
-            curr.Right = split2;
-            curr.Value = -1;
+            curr.Left = new Tree(curr, true) 
+            {  
+                Value = curr.Value / 2
+            };
+            curr.Right = new Tree(curr, false) 
+            { 
+                Value = (curr.Value + 1) / 2 
+            };
+            curr.Value = null;
 
             return true;
         }
@@ -164,91 +120,118 @@ static bool Split(Tree tree)
     return false;
 }
 
-static Tree Parse(string s)
-{
-    Tree root = new Tree();
-
-    Tree currNode = root;
-    var enumerator = s.GetEnumerator();
-    enumerator.MoveNext();
-    do
-    {
-        if (enumerator.Current == '[')
-        {
-            Tree child = new Tree();
-            child.Parent = currNode;
-            child.IsLeft = true;
-            currNode.Left = child;
-            currNode = child;
-            enumerator.MoveNext();
-            continue;
-        }
-        if (Char.IsDigit(enumerator.Current))
-        {
-            List<char> value = new List<char>();
-            do
-            {
-                value.Add(enumerator.Current);
-                enumerator.MoveNext();
-            }
-            while (enumerator.Current != ',' && enumerator.Current != ']');
-
-            currNode.Value = int.Parse(new String(value.ToArray()));
-            currNode = currNode.Parent;
-            continue;
-        }
-        if (enumerator.Current == ',')
-        {
-            Tree child = new Tree();
-            child.Parent = currNode;
-            child.IsRight = true;
-            currNode.Right = child;
-            currNode = child;
-            enumerator.MoveNext();
-            continue;
-        }
-        if (enumerator.Current == ']')
-        {
-            currNode = currNode.Parent;
-            if (!enumerator.MoveNext())
-            {
-                break;
-            }
-            continue;
-        }
-    }
-    while (true);
-
-    return root;
-}
-
 public class Tree
 {
-    public int Value { get; set; }
-    public Tree Parent { get; set; }
-    public Tree Left { get; set; }
-    public Tree Right { get; set; }
+    public Tree() { }
+    public Tree(Tree parent, bool isLeft)
+    {
+        this.Parent = parent;
+        this.IsLeft = isLeft;
+        this.IsRight = !isLeft;
+    }
+
+    public int? Value { get; set; }
+    public Tree? Parent { get; set; }
+    public Tree? Left { get; set; }
+    public Tree? Right { get; set; }
     public bool IsLeft { get; set; }
     public bool IsRight { get; set; }
+
+    public Tree FindLeft() => find(true);
+    public Tree FindRight() => find(false);
+
+    private Tree find(bool left)
+    {
+        var curr = this;
+        while (curr != null && ((left && curr.IsLeft) || (!left && curr.IsRight)))
+        {
+            curr = curr.Parent;
+        }
+        curr = left ? curr?.Parent?.Left : curr?.Parent?.Right;
+
+        while ((left && curr?.Right != null) || (!left && curr?.Left != null))
+        {
+            curr = left ? curr.Right : curr.Left;
+        }
+
+        return curr;
+    }
+
+    public int Magnitude()
+    {
+        if (this.Left == null && this.Right == null)
+        {
+            return this.Value.Value;
+        }
+        return 3 * this.Left.Magnitude() + 2 * this.Right.Magnitude();
+    }
+    public static Tree Parse(string str)
+    {
+        Tree root = new Tree();
+
+        Tree currNode = root;
+        var enumerator = str.GetEnumerator();
+        enumerator.MoveNext();
+        while (true)
+        {
+            switch (enumerator.Current)
+            {
+                case '[':
+                    Tree left = new Tree(currNode, true);
+                    currNode.Left = left;
+                    currNode = left;
+                    enumerator.MoveNext();
+                    break;
+                case ',':
+                    Tree right = new Tree(currNode, false);
+                    currNode.Right = right;
+                    currNode = right;
+                    enumerator.MoveNext();
+                    continue;
+                    break;
+                case ']':
+                    currNode = currNode.Parent;
+                    if (!enumerator.MoveNext()) goto quit;
+                    break;
+                case var ch:
+                    List<char> value = new List<char>();
+                    do
+                    {
+                        value.Add(enumerator.Current);
+                        enumerator.MoveNext();
+                    }
+                    while (Char.IsDigit(enumerator.Current));
+
+                    currNode.Value = int.Parse(new string(value.ToArray()));
+                    currNode = currNode.Parent;
+                    break;
+            }
+        }
+    quit:;
+
+        return root;
+
+    }
+
     public override string ToString()
     {
         if (this.Left == null && this.Right == null)
         {
             return this.Value.ToString();
         }
-        List<string> strings = new List<string>();
+        StringBuilder sb = new StringBuilder();
         if (this.Left != null)
         {
-            strings.Add("[");
-            strings.Add(this.Left.ToString());
-            strings.Add(",");
+            sb.Append("[");
+            sb.Append(this.Left.ToString());
+            sb.Append(",");
         }
         if (this.Right != null)
         {
-            strings.Add(this.Right.ToString());
-            strings.Add("]");
+            sb.Append(this.Right.ToString());
+            sb.Append("]");
         }
 
-        return strings.Aggregate(new StringBuilder(), (sb, str) => sb.Append(str), sb => sb.ToString());
+        return sb.ToString();
     }
 }
